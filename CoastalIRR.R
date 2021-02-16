5315e455bb6f2c61d55be2d27e37f75cd1777996
Evan B. Goldstein <ebgoldstein@users.noreply.github.com>
2021-02-16 13:01:59 -0500

    file moving parts

--- CoastalIRR.R
+++ CoastalIRR.R
@@ -150,7 +150,7 @@ IRR_results
 
 
 # Isolate experiment 1, washtype, and overwash
-Exp1 <- all_pivot_count %>%
+Exp123 <- all_pivot_count %>%
   filter(experimentCount < 4) %>%
   filter(question == "overwash" | question == "washType") %>%
   select("NOAA_flight":"7") %>%
@@ -159,10 +159,66 @@ Exp1 <- all_pivot_count %>%
          `7` = as.numeric(`7`))
 
 #Counts for each
-Exp1$OWcount <- rowSums(Exp1[,4:10])
+Exp123$OWcount <- rowSums(Exp1[,4:10])
 
 #Bar chart facet
-ggplot(Exp1, aes(OWcount)) + geom_bar() + facet_wrap(~question)
+ggplot(Exp123, aes(OWcount)) + geom_bar() + facet_wrap(~question)
 
 #Bar chart dodge
-ggplot(Exp1, aes(OWcount)) + geom_bar(aes(fill = question),position = "dodge") 
+#ggplot(Exp123, aes(OWcount)) + geom_bar(aes(fill = question),position = "dodge") 
+
+##############
+# You can ignore this if you don't want to look at the images.
+########
+#Move images to folders based on counts for both washover deposit and overwash votes.
+#this is for manual, visual inspection..
+
+#MAKE FOLDERS
+#washovr deposit
+dir.create("washType")
+dir.create("washType/0")
+dir.create("washType/1")
+dir.create("washType/2")
+dir.create("washType/3")
+dir.create("washType/4")
+dir.create("washType/5")
+dir.create("washType/6")
+dir.create("washType/7")
+
+#Overwash impact
+dir.create("overwash")
+dir.create("overwash/0")
+dir.create("overwash/1")
+dir.create("overwash/2")
+dir.create("overwash/3")
+dir.create("overwash/4")
+dir.create("overwash/5")
+dir.create("overwash/6")
+dir.create("overwash/7")
+
+#Located the directory of teh images, whichc an be downlaoded with psi-collect or
+#by clicking the links in storms.noaa.gov.
+# regardless, you will need:
+#Florence — 20180917a_jpgs
+#Michael - 20181011a_jpgs
+#isaias - 20200804a_jpgs
+# i put these three directories in a single folder 'IRRpics'... e.g. IRRpics/20180917a_jpgs
+psi_loc = "/Volumes/Passport/IRRpics/"
+
+#Add correct directories as a column to the dataframe
+#add destination directory and source directory and file
+Exp123dest <- Exp123 %>%
+  mutate(dest =  str_c(question,"/", as.character(OWcount))) %>%
+  mutate_all(~gsub("Florence_|Isaias_|Michael_", "", .)) %>% #remove the storm name from flight directory name
+  mutate(src =  str_c(psi_loc, NOAA_flight,"/jpgs/", as.character(image))) 
+
+
+filelist <- Exp123dest$src
+destlist <- file.path(Exp123dest$dest, Exp123dest$image)
+
+#MOVE IMAGES
+#cycle through dataframe and move files
+file.copy(filelist, destlist)
+
+
+
